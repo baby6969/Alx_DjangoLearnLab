43fca6c1d3662f30f290c7588d5eb6381112ecbb
baby6969 <yafetzekarias23@fmail.com>
2025-09-13 17:30:51 -0400

    new file

--- advanced_features_and_security/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/bookshelf/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)

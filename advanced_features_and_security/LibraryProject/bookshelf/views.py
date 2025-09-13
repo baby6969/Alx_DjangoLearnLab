990191342b585bd187d94e9dac39679c10d78bfb
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-09-13 18:21:19 -0400

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
 
 
 

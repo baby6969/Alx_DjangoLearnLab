4690c512809a1dc2e3a319a2dce9e981993b1b3f
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-09-13 18:20:26 -0400

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from .models import Book
 
+# Example form for demonstration
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+
+# ModelForm for your Book model (used in add_book view)
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book

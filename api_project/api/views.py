750ec664de33e1eb7b7120e063585e2f2411a01d
baby6969 <yafetzekarias23@fmail.com>
2025-09-21 16:54:03 -0400

    update

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,15 @@
-from rest_framework import generics
+
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
-class BookList(generics.ListAPIView):
+# Keep your old ListAPIView if needed
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+# New ViewSet for CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

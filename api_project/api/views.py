524ab39e44a38dc8ed52c2bbe06b99a561e3c3fa
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-09-28 04:10:36 -0400

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,7 +5,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView,generics.ListAPIView
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]  # 👈 only logged-in users can view

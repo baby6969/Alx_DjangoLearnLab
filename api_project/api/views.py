81b884ec10956b462a08db02a6edbeadc8c083f6
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-09-28 04:10:14 -0400

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,generics.ListAPIView
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()

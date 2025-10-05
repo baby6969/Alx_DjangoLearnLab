5fd4c378db3f90cefe37416ce1586a9ae71b7214
baby6969 <yafetzekarias23@fmail.com>
2025-10-05 16:41:28 -0400

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('posts/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path("comment/<int:pk>/update/", )
+    path( "post/<int:pk>/comments/new/",)        
+    path( "comment/<int:pk>/delete/")    
 ]

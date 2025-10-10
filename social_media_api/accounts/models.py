c632ca18978671f2baea8a40097c9d6adeb2c60e
baby6969 <yafetzekarias23@fmail.com>
2025-10-10 17:27:12 -0400

    create && update urls.py ,README.md ,Serilaziers.py , setting.py model.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class user (AbstractUser):
+    bio = models.textfield(blank=True)
+    profile_picture =models.Imagefield(updatte_to='profiles/',null=True, blank =True)
+    followers = models.manytomanyfield('self', symmetrical=False, related_name='following', blank=True)
+    def __str__(self):
+        return self.username
+

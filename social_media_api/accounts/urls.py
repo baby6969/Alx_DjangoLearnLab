86ab13cd7c18ac2244bad39b9d551df59cc8f953
baby6969 <yafetzekarias23@fmail.com>
2025-10-12 15:56:44 -0400

    fix_bug account/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,38 +1,16 @@
 from django.urls import path
-from .views import RegisterView, LoginView 
-from .views import FollowUserView, UnfollowUserView, FollowingListView, FollowersListView
-
+from .views import (
+    RegisterView, LoginView, ProfileView,
+    FollowUserView, UnfollowUserView,
+    FollowingListView, FollowersListView
+)
 
 urlpatterns = [
-   path('register/', RegisterView.as_view(), name='register'),
-   path('login/', LoginView.as_view(), name='login'),
-   path('profile/', ProfileView.as_view(), name='profile'),
-   path('follow/', FollowUserView.as_view(), name='follow'),
-   path('unfollow/', UnfollowUserView.as_view(), name='unfollow'),
-   path('following/', FollowingListView.as_view(), name='following-list'),
-   path('followers/', FollowersListView.as_view(), name='followers-list'),
-   path('api/accounts/', include('accounts.urls')),
-   path('api/', include('posts.urls')),
-
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
+    path('following/', FollowingListView.as_view(), name='following-list'),
+    path('followers/', FollowersListView.as_view(), name='followers-list'),
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

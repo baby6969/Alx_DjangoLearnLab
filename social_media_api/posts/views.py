b6de8176758ec30386c998431b9e12ccddcb775d
baby6969 <yafetzekarias23@fmail.com>
2025-10-12 16:21:26 -0400

    create new app notification

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions,generics
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404

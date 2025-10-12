ecc50c7e0c6f801c5781e25cbcaffca073d2954e
baby6969 <yafetzekarias23@fmail.com>
2025-10-12 15:13:25 -0400

    update models

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,28 +1,44 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model, authenticate
 
 
-user = get_user_model()
-class UserSerializer(serializers.ModelSerializer):
-    class meta:
-        model = user
-        fields = ['id','username', 'email', 'bio', 'profile_picture']
+User = get_user_model()
+
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    
-class mata:
-    model =user
-    fields = ['username', 'email', 'password', 'bio', 'profile_picture']
-    
-def create(self, validated_date):
-    password = validated_data.pop(password)
-    user= user (**validated_data)
-    user.set_password(password)
-    user.save()
-    Token.objects.create(user=user)
-    return user
+    password = serializers.CharField(write_only=True, min_length=6)
+    password2 = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, data):
+        # Check if passwords match
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        # Remove password2 before creating the user
+        validated_data.pop('password2')
+        # Create user using Django's built-in method
+        user = User.objects.create_user(**validated_data)
+        return user
+
 
 class LoginSerializer(serializers.Serializer):
- username = serializers.CharField()
- password = serializers.CharField(write_only=True)
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid username or password.")
+
+# ----------------------------
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']

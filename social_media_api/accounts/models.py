e723d8c802f78d70b09a3194630c163397133f54
baby6969 <yafetzekarias23@fmail.com>
2025-10-12 14:54:14 -0400

    update models

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,13 +1,12 @@
-from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class user (AbstractUser):
-    bio = models.TextField(blank=True)
-    profile_picture =models.ImageField(upload_to='profiles/',null=True, blank =True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
-    def __str__(self):
-        return self.username
 
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+
+def __str__(self):
+    return self.username
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
 
+
 urlpatterns = [
-   path('register/', registerViews.as_view(), name= 'register'),
-   path('login',loginViews.as_view() ,name='login'),
+   path('register/', RegisterView.as_view(), name='register'),
+   path('login/', LoginView.as_view(), name='login'),
+   path('profile/', ProfileView.as_view(), name='profile'),
+   ]
 
-
-]
 
 
 
